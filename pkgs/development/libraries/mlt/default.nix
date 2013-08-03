9d8d881500f57d8be1fdfcc2b4cbfacb81d22a8a
Moritz Ulrich <moritz@tarn-vedra.de>
2013-08-03 16:44:47 +0200

    Update mlt and generate python bindings.
    TODO: Make different bindings parametric.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/libraries/mlt/default.nix
+++ pkgs/development/libraries/mlt/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, SDL, ffmpeg, libdv, libsamplerate, libvorbis
-, libxml2 , pkgconfig, qt4, sox, gtk2 }:
+, libxml2 , pkgconfig, qt4, sox, gtk2, python, swig, pkgs }:
 
 stdenv.mkDerivation rec {
   name = "mlt-${version}";
@@ -12,14 +12,20 @@ stdenv.mkDerivation rec {
 
   buildInputs =
     [ SDL ffmpeg libdv libsamplerate libvorbis libxml2 pkgconfig qt4
-      sox # gtk2 /*optional*/
+      sox python swig pkgs.which # gtk2 /*optional*/
     ];
 
   # Mostly taken from:
   # http://www.kdenlive.org/user-manual/downloading-and-installing-kdenlive/installing-source/installing-mlt-rendering-engine
-  configureFlags = [ "--enable-gpl" "--enable-gpl3" "--avformat-swscale" ];
+  configureFlags = [ "--enable-gpl" "--enable-gpl3" "--avformat-swscale" "--swig-languages=python" ];
 
   enableParallelBuilding = true;
+
+  postInstall = ''
+    mkdir -p $out/lib/${python.libPrefix}/site-packages/
+    cp -v src/swig/python/mlt.py $out/lib/${python.libPrefix}/site-packages/
+    cp -v src/swig/python/_mlt.so $out/lib/${python.libPrefix}/site-packages/
+  '';
 
   meta = {
     homepage = http://www.mltframework.org/;
