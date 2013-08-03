bbc824c47ef5489b4dbb01f213512122d8ef778d
Moritz Ulrich <moritz@tarn-vedra.de>
2013-08-03 16:45:49 +0200

    Update pycairo to 1.10.0
    This was a desperate attempt to fix pygoocanvas's segfaulting.
    
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- pkgs/development/python-modules/pycairo/default.nix
+++ pkgs/development/python-modules/pycairo/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchurl, python, pkgconfig, cairo, x11 }:
 
-stdenv.mkDerivation {
-  name = "pycairo-1.8.8";
+stdenv.mkDerivation rec {
+  name = "pycairo-1.10.0";
   src = fetchurl {
-    url = http://cairographics.org/releases/pycairo-1.8.8.tar.gz;
+    url = "http://cairographics.org/releases/${name}.tar.gz";
     sha256 = "0q18hd4ai4raljlvd76ylgi30kxpr2qq83ka6gzwh0ya8fcmjlig";
   };
 
