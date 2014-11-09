2fb5dbdd7f77f77de88a00e75377323218c04830
Moritz Ulrich <moritz@tarn-vedra.de>
2014-11-09 12:17:49 +0100

    Chicken: Fix include-path in setup-hook.

--- pkgs/development/compilers/chicken/overrides.nix
+++ pkgs/development/compilers/chicken/overrides.nix
@@ -2,7 +2,7 @@
   setup-helper = {
     preBuild = ''
       substituteInPlace setup-helper.setup \
-        --replace "(chicken-home)" \"$out/share/\"
+        --replace "(chicken-home)" \"$out/share/chicken/\"
 
         cat setup-helper.setup
     '';
