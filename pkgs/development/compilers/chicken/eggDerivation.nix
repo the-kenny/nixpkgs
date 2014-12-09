a9abb98dc343643bb6ebd7568775ae1478a43fe0
Moritz Heidkamp <moritz@twoticketsplease.de>
2014-12-09 23:12:10 +0100

    Rework eggDerivation so that it can handle the schematic egg

--- pkgs/development/compilers/chicken/eggDerivation.nix
+++ pkgs/development/compilers/chicken/eggDerivation.nix
@@ -23,9 +23,7 @@ stdenv.mkDerivation ({
   buildInputs = [ makeWrapper ];
 
   CSC_OPTIONS = stdenv.lib.concatStringsSep " " cscOptions;
-
   CHICKEN_REPOSITORY = libPath;
-  CHICKEN_INSTALL_PREFIX = "$out";
 
   # We override the default buildPhase because some eggs ship a
   # Makefile which we need to skip over.
@@ -38,13 +36,33 @@ stdenv.mkDerivation ({
   installPhase = ''
     runHook preInstall
 
-    chicken-install -p $out ${stdenv.lib.concatStringsSep " " chickenInstallFlags}
+    # Some eggs need to be able to access modules installed by
+    # themselves at compile time so we add the current egg's
+    # repository path to CHICKEN_REPOSITORY_EXTRA here
+    EGG_CHICKEN_REPOSITORY="$out/lib/chicken/${toString chicken.binaryVersion}"
+    mkdir -p $EGG_CHICKEN_REPOSITORY
+    addToSearchPath CHICKEN_REPOSITORY_EXTRA "$EGG_CHICKEN_REPOSITORY"
 
+    # We need the chicken-install wrapper to pass the -p option since
+    # CHICKEN_INSTALL_PREFIX seems to be broken and some eggs' setup
+    # file call chicken-install again (e.g. schematic's).
+    NIX_SUPPORT_BIN=$out/nix-support/bin/
+    mkdir -p "$NIX_SUPPORT_BIN"
+    makeWrapper ${chicken}/bin/chicken-install $NIX_SUPPORT_BIN/chicken-install --add-flags "-p $out"
+
+    # Finally, build and install the egg
+    PATH="$NIX_SUPPORT_BIN:$PATH" chicken-install ${stdenv.lib.concatStringsSep " " chickenInstallFlags}
+
+    # Remove wrapper again
+    rm $NIX_SUPPORT_BIN/chicken-install
+    rmdir --ignore-fail-on-non-empty -p $NIX_SUPPORT_BIN
+
+    # Wrap all installed programs appropriately
     for f in $out/bin/*
     do
       wrapProgram $f \
         --set CHICKEN_REPOSITORY $CHICKEN_REPOSITORY \
-        --prefix CHICKEN_REPOSITORY_EXTRA : "$out/lib/chicken/${toString chicken.binaryVersion}/:$CHICKEN_REPOSITORY_EXTRA" \
+        --prefix CHICKEN_REPOSITORY_EXTRA : "$CHICKEN_REPOSITORY_EXTRA" \
         --prefix CHICKEN_INCLUDE_PATH \; \"$CHICKEN_INCLUDE_PATH\;$out/share/chicken\" \
         --prefix PATH : "$out/bin:${chicken}/bin:$CHICKEN_REPOSITORY_EXTRA:$CHICKEN_REPOSITORY"
     done
