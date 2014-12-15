1bb9a0b4af443a270522238fd3f259bb0981785c
Moritz Ulrich <moritz@tarn-vedra.de>
2014-12-15 12:45:02 +0100

    eggDerivation: Fix setup-hook.

--- pkgs/development/compilers/chicken/setup-hook.sh
+++ pkgs/development/compilers/chicken/setup-hook.sh
@@ -1,8 +1,15 @@
 addChickenRepositoryPath() {
     addToSearchPathWithCustomDelimiter : CHICKEN_REPOSITORY_EXTRA "$1/lib/chicken/7/"
     # addToSearchPathWithCustomDelimiter \; CHICKEN_INCLUDE_PATH "$1/share/"
-    export CHICKEN_INCLUDE_PATH="$1/share/chicken;$CHICKEN_INCLUDE_PATH"
-    export CSC_OPTIONS="-I$1/include/chicken -L$1/lib $CSC_OPTIONS"
+    if [ -d "$1/share/chicken" ]; then
+        export CHICKEN_INCLUDE_PATH="$1/share/chicken;$CHICKEN_INCLUDE_PATH"
+    fi
+    if [ -d "$1/share/include" ]; then
+        export CSC_OPTIONS="-I$1/include/chicken $CSC_OPTIONS"
+    fi
+    if [ -d "$1/lib" ]; then
+        export CSC_OPTIONS="-L$1/lib $CSC_OPTIONS"
+    fi
 }
 
 envHooks=(${envHooks[@]} addChickenRepositoryPath)
