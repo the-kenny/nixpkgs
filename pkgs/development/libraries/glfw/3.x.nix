25d8e3e2f521e27b3617b18643d4ff750f970951
Moritz Ulrich <moritz@tarn-vedra.de>
2015-06-23 00:57:59 +0200

    Fix merge-induced error.

--- pkgs/development/libraries/glfw/3.x.nix
+++ pkgs/development/libraries/glfw/3.x.nix
@@ -19,8 +19,6 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = "-DBUILD_SHARED_LIBS=ON";
 
-  cmakeFlags = "-DBUILD_SHARED_LIBS=ON";
-
   meta = with stdenv.lib; { 
     description = "Multi-platform library for creating OpenGL contexts and managing input, including keyboard, mouse, joystick and time";
     homepage = "http://glfw.sourceforge.net/";
